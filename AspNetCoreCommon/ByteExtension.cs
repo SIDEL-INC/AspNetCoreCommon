38b439381ba2d5599d5663d9c19d40ed09d0a8c1
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2021-11-20 20:55:28 +0900

    Primitive Extensions

---
+++ AspNetCoreCommon/ByteExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class ByteExtension
+    {
+        public static string ToUtf8String(this byte[] bytes)
+        {
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        public static string ToHexString(this byte[] bytes)
+        {
+            return BitConverter.ToString(bytes).Replace("-", string.Empty);
+        }
+
+        public static string ToBase64(this byte[] bytes)
+        {
+            return Convert.ToBase64String(bytes);
+        }
+
+        public static byte[] Padding(this byte[] bytes, int size)
+        {
+            if (bytes.Length < size)
+            {
+                var originalBytes = bytes.Length;
+                var remainBytes = size - originalBytes;
+
+                Array.Resize(ref bytes, size);
+                for (var i = originalBytes - 1; i < remainBytes; ++i)
+                {
+                    bytes[i] = (byte)(remainBytes & 0xFF);
+                }
+            }
+
+            return bytes;
+        }
+    }
+}
---
+++ AspNetCoreCommon/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static byte[] AsUtf8(this string str)
+        {
+            return Encoding.UTF8.GetBytes(str);
+        }
+
+        public static byte[] AsBase64Bytes(this string str)
+        {
+            return Convert.FromBase64String(str);
+        }
+
+        public static string Hash(this string str)
+        {
+            return str.AsUtf8().ToHexString();
+        }
+    }
+}
